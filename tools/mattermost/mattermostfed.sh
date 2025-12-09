d09f158d2a4c36a9abeecc5296587de2670fdc72
ChrisFickess <CFickess20@gmail.com>
2025-12-09 15:18:25 -0600

    Updating a function for aws connection and changing the version of teleport in a new env file for all tools folders

--- tools/mattermost/mattermostfed.sh
+++ tools/mattermost/mattermostfed.sh
@@ -3,15 +3,6 @@
 # This is for Mattermost Org specific bash functions and settings
 # This is set in the tmp/env.sh file as MATTERMOSTFED=TRUE
 
-export MMCTL_URL="https://releases.mattermost.com/mmctl/${MMCTL_RELEASE_VERSION}/linux_amd64.tar"
-export MATTERMOST_API_URL="https://federation.mattermost.com/api/v4"
-export MATTERMOST_SITE_URL="https://federation.mattermost.com"
-
-# # Versions
-export MMCTL_RELEASE_VERSION="v11.1.0"
-export MMCTL_PREVIOUS_VERSION="v11.1.0"
-
-
 # ------------------
 # Secret Functions
 # ------------------
