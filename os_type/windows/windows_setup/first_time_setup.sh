30b731a5784b710d9970439053dad27099f82537
christopher.a.fickess <christopher.a.fickess@boeing.com>
2026-02-01 12:05:55 -0600

    Testing Windows Install issues

--- os_type/windows/windows_setup/first_time_setup.sh
+++ os_type/windows/windows_setup/first_time_setup.sh
@@ -23,11 +23,14 @@ function __install_software_windows__() {
         echo -e "${YELLOW}   To Continue with User Installation, Press Enter...${NC}"
         read -r
         __winget_install_tools__
-    elif [[  ! "$admin_confirm" =~ ^[Yy]$ ]]; then
+    elif [[  ! "$admin_confirm" =~ ^[Nn]$ ]]; then
         echo -e "${GREEN}User confirmed Admin Terminal. Proceeding with Admin Installation...${NC}"
         echo -e "${YELLOW}   To Continue with Admin Installation, Press Enter...${NC}"
         read -r
         __choco_install_tools__
+    else
+        echo -e "${RED}Invalid input. Please enter 'y' or 'n'.${NC}"
+        __install_software_windows__
     fi
 }
 
